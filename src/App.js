691b1bb69fd503a4ce693e5174a1ac16ab710f08
[HRCEO] <[kw8989@naver.com]>
2021-11-08 11:19:42 +0900

    react CRUD project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import Axios from "axios";
+
+const server_url = 'http://localhost:3001/';
 
 function App() {
+
+  const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
+
+
+  useEffect(()=>{
+      Axios.get(`${server_url}api/get`).then((response)=>{
+          console.log(response);
+      })
+  },[])
+
+  const submitReview = () =>{
+    console.log('user submit');
+      Axios.post(`${server_url}api/insert`,
+          {
+              movieName:movieName,
+              movieReview:review
+          }).then((result)=>{
+              alert('successful insert');
+          console.log(result + 'test front');
+      }).catch((err)=>{
+          console.log(err);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD APPLICATION</h1>
+      <div className="from">
+          <label>Movie Name : </label>
+        <input type="text" name="movieName" onChange={(e)=>{
+            setMovieName(e.target.value);
+        }}/>
+          <label>Review : </label>
+        <input type="text" name="review" onChange={(e)=>{
+            setReview(e.target.value)}}/>
+          <button onClick={submitReview}>Submit</button>
+      </div>
     </div>
   );
 }
